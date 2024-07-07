3cb95e9015d97970fcf650c6106cf671a6de54aa
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-07-07 20:26:54 +0200

    Add Server filtering for comments

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -3,13 +3,16 @@ import * as request from "../lib/request";
 const baseUrl = "http://localhost:3030/data/comments";
 
 export const getGameComments = async (gameId) => {
-  /* const query = new URLSearchParams({
+  const query = new URLSearchParams({
     where: `gameId="${gameId}"`,
-  }); */
-  const result = await request.get(`${baseUrl}`);
+  });
+  // server filtering
+  const result = await request.get(`${baseUrl}?${query}`);
 
   // TODO: temp solution until migration to collections service
-  return Object.values(result).filter((comment) => comment.gameId === gameId);
+  // return result.filter((comment) => comment.gameId === gameId);
+
+  return result;
 };
 
 export const create = async (gameId, username, text) => {
